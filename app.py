e5086101a5768280558c5b47311e4e3ee5b92be1
jjmvago <jjmvago@gmail.com>
2024-11-28 14:08:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from langchain_core.messages import SystemMessage, HumanMessage, trim_messages
 import uuid
 import json
 from operator import itemgetter
+from langchain_openai import ChatOpenAI
 
 # Load environment variables
 load_dotenv()
@@ -24,14 +25,7 @@ os.environ["GROQ_API_KEY"] = st.secrets["GROQ_API_KEY"]
 
 # Create the model
 #llm = ChatGroq(model="Gemma2-9b-It", temperature=0.6)
-try:
-    llm = ChatGroq(model="Gemma2-9b-It", temperature=0.6)
-    response = llm.generate({"messages": [{"role": "user", "content": "Hello!"}]})
-    print(response)
-except Exception as e:
-    import traceback
-    print("Error initializing ChatGroq or generating response:")
-    print(traceback.format_exc())
+llm = ChatOpenAI()
 
 # Define the chat prompt template
 prompt = ChatPromptTemplate(
